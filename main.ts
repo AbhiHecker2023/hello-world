4d9bed0cea0343524d61e94faae96237a53b43fb
AbhiHecker2023 <122548006+AbhiHecker2023@users.noreply.github.com>
2023-01-13 11:16:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Breve))
     basic.showIcon(IconNames.Rabbit)
 })
 input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    music.playTone(698, music.beat(BeatFraction.Breve))
     basic.showIcon(IconNames.TShirt)
 })
 basic.showIcon(IconNames.Ghost)
