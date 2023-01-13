d02d60a684f722e61d8c813ff0e12e43a68687db
AbhiHecker2023 <122548006+AbhiHecker2023@users.noreply.github.com>
2023-01-13 11:23:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Rabbit)
 })
 input.onButtonPressed(Button.B, function () {
-    music.playMelody("E B C5 A B G A F ", 120)
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("E B C5 A B G A F ", 120)
+    }
 })
 basic.showIcon(IconNames.Ghost)
