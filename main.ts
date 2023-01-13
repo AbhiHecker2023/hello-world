0a051e8e56a72497ef9986a338d3fb19863365bd
AbhiHecker2023 <122548006+AbhiHecker2023@users.noreply.github.com>
2023-01-13 11:15:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
     basic.showIcon(IconNames.Rabbit)
 })
 input.onButtonPressed(Button.B, function () {
