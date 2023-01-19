ba7e53008a2455a427b1a681fefaff10ca5c4950
AbhiHecker2023 <122548006+AbhiHecker2023@users.noreply.github.com>
2023-01-19 11:11:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(262, music.beat(BeatFraction.Breve))
-    basic.showIcon(IconNames.Rabbit)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        music.playMelody("E B C5 A B G A F ", 120)
-    }
+    basic.showIcon(IconNames.Heart)
 })
-music.setVolume(255)
-basic.showIcon(IconNames.Ghost)
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Hello!")
+})
